34b8958b5708ff3a18ac761ec26589e131fc241a
Yuri Martinovsky <vvrite@me.com>
2021-06-11 13:04:15 +0300

    Initial Commit

---
+++ Contact List App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Contact List App
+//
+//  Created by Yuriy Martinovskiy on 11.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
